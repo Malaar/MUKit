7437e12800475579e1a76256c4487630f8f4de5d
Malaar <korshilovskiy@gmail.com>
2012-04-02 14:59:34 +0300

    restruct

---
+++ MUKit/MUTableDisposer/CellDatas/CellDataSimple/MUCellDataLabel.h
@@ -0,0 +1,21 @@
+//
+//  MUTextCellData.h
+//  MUKitTest
+//
+//  Created by Yuriy Bosov on 3/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MUCellDataMaped.h"
+
+@interface MUCellDataLabel : MUCellDataMaped
+
+@property (nonatomic, retain) NSString *title;
+@property (nonatomic, retain) UIColor *titleColor;
+@property (nonatomic, retain) UIFont *titleFont;
+
+@property (nonatomic, retain) NSString *text;
+@property (nonatomic, retain) UIColor *textColor;
+@property (nonatomic, retain) UIFont *textFont;
+
+@end
